8dd259a180bceba0267c8e63f90728744b13fd98
Schnisch <holzemil06@gmail.com>
2025-05-15 07:57:00 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+class Program {
+    string apiUrl = "https://api.churchtools.de/v2.1/"; 
+    string apiToken = "your_api_token_here";
+    string userApiUrl = "";
+    string userApiToken = "";
+    HttpClient client = new HttpClient();
+
+    static void Main()
+    {
+      
+    }
+
+    public void Menu()
+    {
+        Console.WriteLine("--Churchtools Console Management--");
+        Console.WriteLine("Choose an Action:");
+        Console.WriteLine("(1) Set Default API");
+    }
+
+    public void SetDefaultAPI()
+    {
+        Console.WriteLine("Paste your API Link:");
+
+    }
+}
